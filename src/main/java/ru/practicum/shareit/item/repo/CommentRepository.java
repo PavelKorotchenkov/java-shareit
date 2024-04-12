faeb8005d67e160b84e7568cf0811834dacd3d9d
Pavel Korotchenkov <radiel13@gmail.com>
2024-04-12 10:44:33 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/item/repo/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repo/CommentRepository.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.item.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.dto.CommentShort;
 import ru.practicum.shareit.item.model.Comment;
 
