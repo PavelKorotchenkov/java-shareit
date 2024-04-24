e6c86234f865151b8080e0131f8a30ddee1446d9
Pavel Korotchenkov <radiel13@gmail.com>
2024-04-24 09:53:18 +0300

    checkstyle

--- src/test/java/ru/practicum/shareit/request/dto/ItemRequestDtoMapperTest.java
+++ src/test/java/ru/practicum/shareit/request/dto/ItemRequestDtoMapperTest.java
@@ -7,13 +7,8 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
 import ru.practicum.shareit.item.dto.ItemForRequestDto;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.request.model.ItemRequest;
-
-import java.util.Collections;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @JsonTest
 class ItemRequestDtoMapperTest {
@@ -38,22 +33,4 @@ class ItemRequestDtoMapperTest {
 		assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
 		assertThat(result).extractingJsonPathStringValue("$.description").isEqualTo("description");
 	}
-
-	/*@Test
-	void toResponseDto() throws Exception {
-		// Arrange
-		ItemRequest itemRequest = ItemRequest.builder()
-				.id(1L)
-				.description("Test description")
-				.items(Collections.singletonList(new Item()))
-				.build();
-
-		// Act
-		ItemRequestResponseDto responseDto = ItemRequestDtoMapper.toResponseDto(itemRequest);
-
-		// Assert
-		assertNotNull(responseDto);
-		assertEquals(itemRequest.getId(), responseDto.getId());
-		assertEquals(itemRequest.getDescription(), responseDto.getDescription());
-	}*/
 }
